73e729e11c00606a79eeaa7370c9a2da799b6329
Travis Heavener <travis.heavener@gmail.com>
2023-12-30 16:21:53 -0500

    Moved --text-unit to toolbox

--- web/php/importAssets.php
+++ web/php/importAssets.php
@@ -3,13 +3,13 @@
     // adds all necessary HTML tags for content (ie. jQuery, global .js files, etc.)
     
     // jQuery
-    echo "<script src=\"https://code.jquery.com/jquery-3.7.1.slim.js\" integrity=\"sha256-UgvvN8vBkgO0luPSUl2s8TIlOSYRoGFAX4jlCIm9Adc=\" crossorigin=\"anonymous\"></script>";
+    echo "<script src='https://code.jquery.com/jquery-3.7.1.slim.js' integrity='sha256-UgvvN8vBkgO0luPSUl2s8TIlOSYRoGFAX4jlCIm9Adc=' crossorigin='anonymous'></script>";
 
     // happy worm favicon
-    echo "<link rel=\"icon\" href=\"/assets/favicon.ico\">";
+    echo "<link rel='icon' href='/assets/favicon.ico'>";
     
     // global css & such
-    echo "<link rel=\"stylesheet\" href=\"/css/toolbox.css\"></script>";
+    echo "<link rel='stylesheet' href='/css/toolbox.css'></script>";
 
     // add the text unit size to the global :root pseudo selector
     echo "<script src='/js/toolbox.js'></script>";

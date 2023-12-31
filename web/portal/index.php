1da3847f563c0ecf998da67fe2dea731269dcc8a
Travis Heavener <travis.heavener@gmail.com>
2023-12-30 22:47:43 -0500

    Added basic account page

--- web/portal/index.php
+++ web/portal/index.php
@@ -15,9 +15,36 @@
     </head>
     <body>
 
-        <div id="anim-controls">
-            <p id="anim-desc">Animate:</p>
-            <input type="checkbox" id="anim-checkbox" onclick="toggleAnim(this)" title="Toggle background animation">
+        <div id="header-content">
+            <img id="logo-img" src="/assets/happy-worm.png" alt="Happy worm icon." title="Home" onclick="window.open('/portal/index.php', '_self')">
+            <img id="my-account-img" src="/assets/profile-icon.png" alt="My Account icon." title="My Account" onclick="window.open('/portal/account/index.php', '_self')">
+        </div>
+
+        <div id="apps-content">
+            <div class="app-icon"></div>
+            <div class="app-icon"></div>
+            <div class="app-icon"></div>
+            <div class="app-icon"></div>
+            <div class="app-icon"></div>
+            <div class="app-icon"></div>
+        </div>
+
+        <div id="footer-content">
+            <?php
+                // pick random footnote
+                $notes = [
+                    "Made with ❤.",
+                    "Est. 2023.",
+                    "Never share your password.",
+                    "<a href='mailto:travis.heavener@gmail.com'>travis.heavener@gmail.com</a> for inquiries."
+                ];
+                echo "<p>" . $notes[array_rand($notes)] ."</p>";
+            ?>
+
+            <div id="anim-controls">
+                <p id="anim-desc">Animate:</p>
+                <input type="checkbox" id="anim-checkbox" onclick="toggleAnim(this)" title="Toggle background animation">
+            </div>
         </div>
 
         <script src="/js/portal/index.js"></script>

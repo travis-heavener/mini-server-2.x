67a9fa9b728c54ae73931244b95d0e27970ba37c
Travis Heavener <travis.heavener@gmail.com>
2024-01-01 22:28:20 -0500

    Fixed header padding

--- web/js/portal/index.js
+++ web/js/portal/index.js
@@ -1,14 +1,4 @@
 // document onready events
 $(document).ready(() => {
     toggleAnim();
-
-    // update header width
-    const updateWidth = () => {
-        const width = $("html").prop("clientWidth");
-        $("body").css("width", width + "px");
-        $("html").css("width", width + "px");
-    };
-
-    $(window).on("resize", updateWidth);
-    updateWidth();
 });

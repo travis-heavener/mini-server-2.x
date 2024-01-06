cd13cdcf3841e4bbac6c40a35b8952c2da5f6920
Travis Heavener <travis.heavener@gmail.com>
2024-01-05 22:58:31 -0500

    Added app clicks

--- web/php/portal/createIcons.php
+++ web/php/portal/createIcons.php
@@ -2,7 +2,7 @@
     // create icons for portal page
     function create_icon($name, $title) {
         return "
-            <div class='app-container noselect'>
+            <div class='app-container noselect' onclick=\"launchApp('$name')\">
                 <div id='$name-icon' class='app-badge'>
                     <img class='app-icon' src='/assets/app-icons/$name.png' type='image/png' alt='$title icon.'>
                 </div>
@@ -11,9 +11,22 @@
         ";
     }
 
+    // determine what permissions the user has to see what apps they can use
+    /*
+    1: yes, 0: no
+    all permissions would have a permissions value of the sum of each 2^(bit #)
+
+    Bit app/permission
+    0   gallery
+    1   dvds
+    2   fire
+    3   clock
+    4   todo
+    */
+
     echo create_icon("gallery", "Gallery");
     echo create_icon("dvds", "Film Library");
     echo create_icon("fire", "Fireplace");
     echo create_icon("clock", "Clock");
-    echo create_icon("todo", "To-Do");
+    echo create_icon("notes", "Notes");
 ?>
--- web/portal/index.js
+++ web/portal/index.js
@@ -2,3 +2,21 @@
 $(document).ready(() => {
     toggleAnim();
 });
+
+// app click functions
+function launchApp(name) {
+    switch (name) {
+        case "clock":
+            location.href = "/portal/apps/clock/index.php";
+            break;
+        case "fire":
+            location.href = "/portal/apps/fireplace/index.php";
+            break;        
+        case "notes":
+            location.href = "/portal/apps/notes/index.php";
+            break;
+        default:
+            console.log("Unknown app/service:", name);
+            break;
+    }
+}

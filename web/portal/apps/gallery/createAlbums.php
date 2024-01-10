582f298b7729f473a4869f72cb469ede5c80727e
Travis H <travis.heavener@gmail.com>
2024-01-10 03:14:44 -0500

    Fixed SQL

--- web/portal/apps/gallery/createAlbums.php
+++ web/portal/apps/gallery/createAlbums.php
@@ -6,7 +6,7 @@
         $mysqli = new mysqli($envs["HOST"], $envs["USER"], $envs["PASS"], $envs["DBID"]);
         
         $statement = $mysqli->prepare(
-            "SELECT `id`, `name`, `album_name`, `MIME`, `vector` FROM `gallery` WHERE `created` IN (SELECT MAX(`created`) FROM `gallery` WHERE `user_id`=? GROUP BY `album_name`) ORDER BY `created` DESC"
+            "SELECT `id`, `name`, `album_name`, `MIME`, `vector` FROM `gallery` WHERE `id` IN (SELECT `id` FROM (SELECT MAX(`created`), MAX(`id`) AS `id` FROM `gallery` WHERE `user_id`=? GROUP BY `album_name`)d) ORDER BY `created` DESC"
         );
         $statement->bind_param("i", $user_id);
         $statement->execute();
@@ -15,6 +15,8 @@
 
         $statement->close();
         $mysqli->close();
+
+        // filter out any duplicate entries ()
 
         // add album previews
         for ($i = 0; $i < count($rows); $i++) {
@@ -32,6 +34,22 @@
             }
         }
 
+        return $rows;
+    }
+
+    function get_full_album($user_id, $album_name) {
+        // load up mysqli
+        $envs = parse_ini_file(dirname($_SERVER['DOCUMENT_ROOT']) . "/config/.env");
+        $mysqli = new mysqli($envs["HOST"], $envs["USER"], $envs["PASS"], $envs["DBID"]);
+        
+        $statement = $mysqli->prepare("SELECT `id`, `name`, `MIME`, `vector` FROM `gallery` WHERE `user_id`=? AND `album_name`=? ORDER BY `created` DESC");
+        $statement->bind_param("is", $user_id, $album_name);
+        $statement->execute();
+
+        $rows = $statement->get_result()->fetch_all(MYSQLI_ASSOC);
+
+        $statement->close();
+        $mysqli->close();
         return $rows;
     }
 
@@ -54,5 +72,29 @@
 
     // display first album, if there are albums
     echo "<div id='album-content'>";
+    if (count($user_albums) > 0) {
+        $envs = parse_ini_file(dirname($_SERVER['DOCUMENT_ROOT']) . "/config/.env");
+        $key = $user_data["aes"];
+        $album = get_full_album($user_data["id"], $user_albums[0]["album_name"]);
+
+        foreach ($album as $media) {
+            $path = $envs["GALLERY_PATH"] . dechex($media["id"]) . ".bin";
+            $MIME = $media["MIME"];
+            if (!file_exists($path) || (!str_starts_with($MIME, "image") && !str_starts_with($MIME, "video"))) {
+                echo "<img src='/assets/app-icons/gallery.png' class='default-icon' alt='Album icon image.'>\n";
+                continue;
+            }
+
+            $data = file_get_contents($path);
+            $raw = openssl_decrypt($data, "aes-256-ctr", $key, $options=0, $media["vector"]);
+
+            if (str_starts_with($MIME, "image")) { // display image
+                $src = "data:$MIME;base64," . base64_encode($raw);
+                echo "<img src='$src' class='album-icon-img' alt='Album image.'>";
+            } else if (str_starts_with($MIME, "video")) { // display video
+                echo "video stuff";
+            }
+        }
+    }
     echo "</div>";
 ?>

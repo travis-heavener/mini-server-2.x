f5ee3a93c5652628b5b4cc7866ca30c684aa2b5c
Travis H <travis.heavener@gmail.com>
2024-01-22 00:32:08 -0500

    Remove draggable attr from header

--- web/php/createHeader.php
+++ web/php/createHeader.php
@@ -2,13 +2,13 @@
     // create apps headers
     echo "
         <div id='header-content'>
-            <img id='logo-img' src='/assets/happy-worm.png' type='image/png' alt='Happy worm icon.' onclick=\"window.location.href = '/index.php'\">
+            <img id='logo-img' class='noselect' draggable='false' src='/assets/happy-worm.png' type='image/png' alt='Happy worm icon.' onclick=\"window.location.href = '/index.php'\">
     ";
 
     // show apps waffle and profile icon
     echo "
-        <img id='waffle-img' src='/assets/app-icons/waffle.png' type=\"image/png\" alt='Dashboard icon.' onclick=\"window.location.href = '/portal/index.php'\">
-        <img id='my-account-img' src='/assets/app-icons/profile.png' type=\"image/png\" alt='My Account icon.' onclick=\"window.location.href = '/portal/account/index.php'\">
+        <img id='waffle-img' class='noselect' draggable='false' src='/assets/app-icons/waffle.png' type=\"image/png\" alt='Dashboard icon.' onclick=\"window.location.href = '/portal/index.php'\">
+        <img id='my-account-img' class='noselect' draggable='false' src='/assets/app-icons/profile.png' type=\"image/png\" alt='My Account icon.' onclick=\"window.location.href = '/portal/account/index.php'\">
     ";
 
     // close div

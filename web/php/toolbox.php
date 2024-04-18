d693ba7f38f94272baf796ff624a9a861a8d208f
Travis H <travis.heavener@gmail.com>
2024-04-18 18:58:44 -0400

    DocumentRoot hotfix

--- web/php/toolbox.php
+++ web/php/toolbox.php
@@ -1,11 +1,6 @@
 <?php
     // load envs
     function loadEnvs() {
-        // resolve document root if it's a symlink
-        if (is_link($_SERVER["DOCUMENT_ROOT"])) {
-            return parse_ini_file(dirname(readlink($_SERVER["DOCUMENT_ROOT"])) . "/config/.env");
-        } else {
-            return parse_ini_file(dirname($_SERVER["DOCUMENT_ROOT"]) . "/config/.env");
-        }
+        return parse_ini_file(dirname($_SERVER["DOCUMENT_ROOT"]) . "/config/.env");
     }
 ?>

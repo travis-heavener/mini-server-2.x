6044e835834c87d0e8513f6cf24ae822d10859ce
Travis H <travis.heavener@gmail.com>
2024-04-24 17:08:13 -0400

    Files ordered by creation date, desc.

--- web/portal/apps/gallery/preloadContent.php
+++ web/portal/apps/gallery/preloadContent.php
@@ -36,10 +36,10 @@
     $table = TABLE_STEM . dechex($user_id); // we already know the id must be valid since it comes directly from the database
 
     if ($album_name == RECYCLE_BIN_NAME) {
-        $statement = $mysqli->prepare("SELECT `id`, `name`, `mime` FROM `$table` WHERE `deletion_date` IS NOT NULL ORDER BY `uploaded` DESC, `id` DESC LIMIT ? OFFSET ?");
+        $statement = $mysqli->prepare("SELECT `id`, `name`, `mime` FROM `$table` WHERE `deletion_date` IS NOT NULL ORDER BY `created` DESC, `id` DESC LIMIT ? OFFSET ?");
         $statement->bind_param("ii", $max_amt, $offset);
     } else {
-        $statement = $mysqli->prepare("SELECT `id`, `name`, `mime` FROM `$table` WHERE `album_name`=? AND `deletion_date` IS NULL ORDER BY `uploaded` DESC, `id` DESC LIMIT ? OFFSET ?");
+        $statement = $mysqli->prepare("SELECT `id`, `name`, `mime` FROM `$table` WHERE `album_name`=? AND `deletion_date` IS NULL ORDER BY `created` DESC, `id` DESC LIMIT ? OFFSET ?");
         $statement->bind_param("sii", $album_name, $max_amt, $offset);
     }
 
